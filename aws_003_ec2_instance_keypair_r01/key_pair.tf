a93bfc12c4001655ff1ffbcaf511c85083167fb2
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-10-05 08:35:14 +0530

    Generate Pem Key Pair
    Generate Pem Key Pair

---
+++ aws_003_ec2_instance_keypair_r01/key_pair.tf
@@ -0,0 +1,18 @@
+# RSA Private key of size 4096 bits <.pem>
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# RSA Public Key from the above
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+# Storing private key as pem file in Locally
+resource "local_file" "tf-key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "xyz_p1_key.pem"
+}
