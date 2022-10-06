69500eb1f32f748f4cf461276007fe5cd57a6d05
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-10-06 06:10:18 +0530

    Delete aws_001_linux_ec2_instance_r01 directory

--- aws_001_linux_ec2_instance_r01/security_gp.tf
+++
@@ -1,8 +0,0 @@
-#Security Group Setting
-resource "aws_security_group" "testing_sg" {
-  name        = "windows_secu_grp_testing"
-  description = "Allow Sample"
-  tags = {
-    Name = "sg_windows"
-  }
-}
